dd676f95caa9835f15ac4241052cfe698778e544
ziinHua <chhinhua@gmail.com>
2023-01-17 21:26:04 +0700

    User profile model

---
+++ src/main/java/com/amigoscode/awsimageupload/profile/UserProfile.java
@@ -0,0 +1,56 @@
+package com.amigoscode.awsimageupload.profile;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class UserProfile {
+
+    private UUID userProfileId;
+    private String usename;
+    private String userProfileImageLink; //S3 link
+
+    public UserProfile(UUID userProfileId, String usename, String userProfileImageLink) {
+        this.userProfileId = userProfileId;
+        this.usename = usename;
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public UUID getUserProfileId() {
+        return userProfileId;
+    }
+
+    public void setUserProfileId(UUID userProfileId) {
+        this.userProfileId = userProfileId;
+    }
+
+    public String getUsename() {
+        return usename;
+    }
+
+    public void setUsename(String usename) {
+        this.usename = usename;
+    }
+
+    public String getUserProfileImageLink() {
+        return userProfileImageLink;
+    }
+
+    public void setUserProfileImageLink(String userProfileImageLink) {
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfile that = (UserProfile) o;
+        return Objects.equals(userProfileId, that.userProfileId) &&
+                Objects.equals(usename, that.usename) &&
+                Objects.equals(userProfileImageLink, that.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, usename, userProfileImageLink);
+    }
+}
