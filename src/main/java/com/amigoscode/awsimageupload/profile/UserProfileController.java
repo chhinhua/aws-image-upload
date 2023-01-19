4a911f7f412568ae6b395466f574d948e5035b32
ziinHua <chhinhua@gmail.com>
2023-01-19 21:11:33 +0700

    Upload image API & Check list to upload images

--- src/main/java/com/amigoscode/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/amigoscode/awsimageupload/profile/UserProfileController.java
@@ -2,11 +2,12 @@ package com.amigoscode.awsimageupload.profile;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
@@ -22,6 +23,16 @@ public class UserProfileController {
     @GetMapping
     public List<UserProfile> getUserProfile() {
         return userProfileService.getUserProfiles();
+    }
+
+    @PostMapping(
+            path = "{userProfileId}/image/download",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable("userProfileId")UUID userProfileId,
+                                       @RequestParam("file")MultipartFile file) {
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 
 
