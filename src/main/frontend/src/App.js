13f8466af54e1a6754977d205e208ab169bbd2c1
chhinhua <chhinhua@gmail.com>
2023-04-11 00:23:06 +0700

    UI logic to send file to backend & increase servlet max file size

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -25,7 +25,7 @@ const UserProfiles = () => {
         <br />
         <h1>{userProfile.username}</h1>
         <p>{userProfile.userProfileId}</p>
-        <Dropzone />
+        <Dropzone {...userProfile} />
         <br />
         <br />
       </div>
@@ -33,10 +33,31 @@ const UserProfiles = () => {
   });
 };
 
-function Dropzone() {
+function Dropzone({ userProfileId }) {
   const onDrop = useCallback((acceptedFiles) => {
     const file = acceptedFiles[0];
-    console.log(file)
+
+    console.log(file);
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    axios
+      .post(
+        `http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      )
+      .then(() => {
+        console.log("file uploaded successfully");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
